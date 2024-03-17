273777fc5782736a27979493161c6e00b4a26b58
Rishikeshghosh <rishikeshghoshghosh@gmail.com>
2024-03-17 13:56:32 +0530

    fixed a bug

--- app/api/livekit/route.ts
+++ app/api/livekit/route.ts
@@ -1,10 +1,11 @@
 import { AccessToken } from "livekit-server-sdk";
+
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
   try {
-    const room = req.nextUrl?.searchParams?.get("room");
-    const username = req.nextUrl?.searchParams?.get("username");
+    const room = ""; //req.nextUrl.searchParams.get("room");
+    const username = ""; //req.nextUrl.searchParams.get("username");
 
     if (!room) {
       return NextResponse.json(
@@ -31,7 +32,12 @@ export async function GET(req: NextRequest) {
 
     const at = new AccessToken(apiKey, apiSecret, { identity: username });
 
-    at.addGrant({ room, roomJoin: true, canPublish: true, canSubscribe: true });
+    at.addGrant({
+      room,
+      roomJoin: true,
+      canPublish: true,
+      canSubscribe: true,
+    });
     const token = await at.toJwt();
 
     return NextResponse.json({ token });
